db137190d102273d522c3b0f3cdbbe5c1bd5f662
Joonatan <joonatanvark@gmail.com>
2024-10-07 19:08:09 +0300

    update keymap

--- keyboards/bastardkb/charybdis/3x6/keymaps/jovark/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/jovark/keymap.c
@@ -41,8 +41,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define SUPER LCA_T(KC_LALT)
 #define RAISE LT(LAYER_RAISE, KC_SPC)
 #define LOWER LT(LAYER_LOWER, KC_ESC)
-#define PT_A LT(LAYER_POINTER, KC_A)
-#define PT_SCLN LT(LAYER_POINTER, KC_SCLN)
+#define PT_Z LT(LAYER_POINTER, KC_A)
+#define PT_SLSH LT(LAYER_POINTER, KC_SCLN)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    PT_A,    KC_S,    KC_D,    KC_F,   KC_G,       KC_H,    KC_J,    KC_K,    KC_L, PT_SCLN, KC_QUOT,
+       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,       KC_N,    KC_M, KC_COMM,   KC_DOT,KC_SLSH, KC_BSLS,
+       KC_LSFT,    PT_Z,    KC_X,    KC_C,    KC_V,   KC_B,       KC_N,    KC_M, KC_COMM,   KC_DOT,PT_SLSH, KC_BSLS,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   LOWER,   RAISE,   KC_ENT,      SUPER,KC_BSPC
   //                            ╰───────────────────────────╯ ╰──────────────────╯
